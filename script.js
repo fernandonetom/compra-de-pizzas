335ba1a6cb0ef56d6379f512703165dacf17c204
fernando39357 <fernando39357@gmail.com>
2020-06-01 14:56:48 -0300

    Listagem de pizzas

---
+++ script.js
@@ -0,0 +1,23 @@
+const select = (element) => document.querySelector(element);
+const selectAll = (element) => document.querySelectorAll(element);
+
+pizzaJson.map((item, index) => {
+	let pizzaItem = select(".models .pizza-item").cloneNode(true);
+
+	pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+	pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+	pizzaItem.querySelector(
+		".pizza-item--price"
+	).innerHTML = `R$ ${item.price.toFixed(2)}`;
+	pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+	pizzaItem.querySelector("a").addEventListener("click", (e) => {
+		e.preventDefault();
+		select(".pizzaWindowArea").style.opacity = 0;
+		select(".pizzaWindowArea").style.display = "flex";
+		setTimeout(() => {
+			select(".pizzaWindowArea").style.opacity = 1;
+		}, 200);
+	});
+
+	select(".pizza-area").append(pizzaItem);
+});
